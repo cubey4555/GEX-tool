25b8dc979fd6177fabef071106544814001e8487
cubey4555 <cubey4555@gmail.com>
2025-11-30 22:36:03 -0500

    Update spy_gex_streamlit.py

--- spy_gex_streamlit.py
+++ spy_gex_streamlit.py
@@ -41,15 +41,15 @@ st.set_page_config(page_title="SPX/SPY GEX Dashboard", layout="wide")
 st.title("📊 SPX/SPY GEX Dashboard (Interactive)")
     # Put ALL your inputs inside the form
 # --- Symbol dropdown (no typing) ---
-    symbol_choice = st.selectbox("Choose symbol:", ["SPY", "SPX"])
+symbol_choice = st.selectbox("Choose symbol:", ["SPY", "SPX"])
     
-    if symbol_choice == "SPY":
-        yf_symbol = "SPY"
-        options_symbol = "SPY"
+if symbol_choice == "SPY":
+    yf_symbol = "SPY"
+    options_symbol = "SPY"
     
-    elif symbol_choice == "SPX":
-        yf_symbol = "^SPX"
-        options_symbol = "^SPX"
+elif symbol_choice == "SPX":
+    yf_symbol = "^SPX"
+    options_symbol = "^SPX"
 
 
 # Stop execution if the user hasn't pressed the button yet
